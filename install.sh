497135ed69a22fe7debd8f3000fc84da41254f9b
Josh Larsen <2565382+joshlarsen@users.noreply.github.com>
2024-10-02 07:21:09 -0400

    initial shadcn setup

---
+++ install.sh
@@ -0,0 +1,42 @@
+#!/bin/bash
+#
+# Install all packages clean from latest
+
+# deps
+yarn add axios \
+  typescript \
+  vite \
+  vue \
+  vue-router@4
+
+# dev deps
+yarn add -D autoprefixer \
+  class-variance-authority \
+  clsx \
+  eslint \
+  eslint-plugin-tailwindcss \
+  eslint-plugin-vue \
+  moment \
+  radix-vue \
+  tailwind-merge \
+  tailwindcss-animate \
+  tailwindcss \
+  vue-tsc \
+  @eslint/js \
+  @eslint/eslintrc \
+  @headlessui/vue \
+  @heroicons/vue \
+  @radix-icons/vue \
+  @vueuse/core \
+  @tailwindcss/aspect-ratio \
+  @tailwindcss/container-queries \
+  @tailwindcss/forms \
+  @tailwindcss/typography \
+  @tailwindcss/aspect-ratio \
+  @types/node \
+  @typescript-eslint/eslint-plugin \
+  @typescript-eslint/parser \
+  @unovis/ts \
+  @unovis/vue \
+  @vitejs/plugin-vue
+
---
+++ src/App.vue
@@ -0,0 +1,12 @@
+<template>
+  <div class="flex min-h-screen flex-col bg-background">
+    <main class="container">
+      <Dashboard />
+    </main>
+  </div>
+</template>
+
+<script setup lang="ts">
+import Dashboard from './Example.vue'
+</script>
+
